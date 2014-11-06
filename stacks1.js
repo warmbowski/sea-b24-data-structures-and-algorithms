52afe47d0a12b7377307c20addccc562f2e9b1cc
Paul Laskowski <pelaskowski@gmail.com>
2014-11-06 00:29:19 -0800

    first commit: stacks exercise 1 complete

---
+++ stacks1.js
@@ -0,0 +1,28 @@
+var stack = [];
+var unmatched = "";
+var expr = process.argv[2];
+var noun = "parenthesis";
+
+for (i = 0; i < expr.length; i++) {
+  if (expr[i] == "(") {
+    stack.push((i+1).toString());
+  };
+  if (expr[i] == ")") {
+    if (stack.length == 0) {
+      unmatched += ((i+1).toString() + ", ");
+    } else {
+      stack.pop();
+    };
+  };
+};
+
+//if (stack.length == 0) unmatched = unmatched.replace(", ", "")
+unmatched += stack.join(", ")
+if (unmatched.indexOf(",") >= 0) noun = "parentheses";
+
+console.log(expr);
+if (unmatched == "" ) {
+  console.log("There are no unmatched parnetheses");
+} else {
+  console.log("Unmatched " + noun + " at " + unmatched);
+}
